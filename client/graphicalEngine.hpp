55bc56ac7dac34f46cba7bc8b989aeb792ace132
vuongc <christopher.k.vuong@gmail.com>
2016-04-02 17:45:01 +0800

    Add method and attribute to GameEngine class

--- client/graphicalEngine.hpp
+++ client/graphicalEngine.hpp
@@ -2,6 +2,7 @@
 # define GRAPHICAL_ENGINE_HPP_
 
 # include <iostream>
+# include <dlfcn.h> // Add this line is the cpp file after
 
 //
 // GraphicalEngine class with load the graphic library
@@ -10,18 +11,19 @@
 class GraphicalEngine
 {
 private:
-  void _loadLibrary(); // May have to change the prototype later
+  GraphicalEngine _library;
 
 public:
   GraphicalEngine();
   ~GraphicalEngine();
+
+  void loadLibrary(); // May have to change the prototype later
 
   void run();
   void start();
   void close();
 
   void draw();
-
 };
 
 #endif // ! GRAPHICAL_ENGINE_HPP_

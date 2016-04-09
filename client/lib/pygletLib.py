e2dc185659efd00e1facafbe6f4b2fc69f737e57
vuongc <christopher.k.vuong@gmail.com>
2016-04-09 21:01:07 +0800

    Can draw a pseudo input box

--- client/lib/pygletLib.py
+++ client/lib/pygletLib.py
@@ -5,10 +5,6 @@ class PygletLib:
     def __init__(self):
         self.content = {}
 
-        options = { 'center': True }
-        self.add_text('question', 'label', 'Question coming soon', options)
-        self.add_text('answer', 'input_box', 'answer here', { 'center-width': True, 'y': 0 })
-
     # Open a window
     def initWindow(self, width, height):
         self.window = pyglet.window.Window(width, height)
@@ -18,22 +14,34 @@ class PygletLib:
         for key, value in self.content.iteritems():
 
             # Center horizontally and vertically
-            if value['opts']['center']:
+            if 'center' in value['opts'] and value['opts']['center']:
                 value['opts']['x'] = self.window.width//2
                 value['opts']['y'] = self.window.height//2
 
             # Center horizontally
-            if value['opts']['center-width']:
+            if 'center-width' in value['opts'] and value['opts']['center-width']:
                 value['opts']['x'] = self.window.width//2
 
             # Draw label
             if value['type'] == 'label':
                 self.draw_label(value['text'], value['opts']);
 
+            # Add input box
+            if value['type'] == 'input_box':
+                self.draw_quads(value['opts'])
+                self.draw_input_box(value['text'], value['opts']);
+
     # Update content
-    def update(self, content):
-        for key, value in content.iteritems():
-            self.content[key] = value
+    def update(self, instructions):
+        for key, value in instructions.iteritems():
+
+            # Add or update content
+            if value['action'] == 'update':
+                self.content[key] = value['content']
+
+            # Delete content
+            if value['action'] == 'delete':
+                del self.content[key]
 
     # Add a text to containers
     def add_text(self, id, type, text, options):
@@ -44,6 +52,24 @@ class PygletLib:
     def draw_label(self, text, opts):
         document = pyglet.text.decode_text(text)
         pyglet.text.Label(document.text, font_name='Times New Roman', font_size=36, x=opts['x'], y=opts['y'], anchor_x='center', anchor_y='center').draw()
+
+    # Draw input box
+    def draw_input_box(self, text, opts):
+        document = pyglet.text.decode_text(text)
+        pyglet.text.Label(document.text, font_name='Times New Roman', font_size=36, color=(0, 0, 0, 255), x=opts['x'], y=opts['y'], anchor_x='center', anchor_y='center').draw()
+
+    # Draw quads
+    def draw_quads(self, opts):
+        upLeft = (opts['x'] - opts['width']//2, opts['y'] + opts['height']//2)
+        upRight = (opts['x'] + opts['width']//2, opts['y'] + opts['height']//2)
+        downLeft = (opts['x'] - opts['width']//2, opts['y'] - opts['height']//2)
+        downRight = (opts['x'] + opts['width']//2, opts['y'] - opts['height']//2)
+
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                             ('v2f', (upLeft[0], upLeft[1],
+                                      upRight[0], upRight[1],
+                                      downRight[0], downRight[1],
+                                      downLeft[0], downLeft[1])))
 
     # Run pyglet app
     def run(self):

0dafe0586bb7419daffde4d4f2163a6817934389
vuongc <christopher.k.vuong@gmail.com>
2016-04-09 16:07:47 +0800

    Add python file and .gitignore

---
+++ client/graphicEngine.py
@@ -0,0 +1,26 @@
+import importlib
+
+class GraphicalEngine:
+
+    # Load library module, here the name is the file name
+    # and the module the class name
+    def loadLibrary(self, name, module):
+        self.library = getattr(importlib.import_module(name), module)
+
+    # Draw from library
+    def draw(self):
+        self.library.draw()
+
+    # Run the library
+    def run(self):
+        self.library.run()
+
+    # Close library
+    def close(self):
+        self.library.close()
+
+if __name__ == '__main__':
+    gE = GraphicalEngine()
+    gE.loadLibrary('lib.pygletLib', 'PygletLib')
+    gL = gE.library()
+    gL.run()
